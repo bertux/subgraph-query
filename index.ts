c942e67743f3ef36819fec2d3491400d9fec8d65
Bertrand Juglas <b@juglas.name>
2024-05-07 16:33:47 +0200

    adapt to nfts2me subgraph

--- index.ts
+++ index.ts
@@ -2,10 +2,10 @@
 import {ApolloClient, InMemoryCache, gql} from '@apollo/client';
 
 // Step 2:  Define your Query
-export const NGP_QUERY = `query info($address: Bytes) {   noGasPasses(where: {creator: $address}) {     id   } }`;
+export const NFT_QUERY = `query info($address:ID!) { nftcollector(id: $address) { nftsFromCollection(where: {collection_: {id: "0x50B51701B68Dd798eEc4FF40526689eff2540069"}}) { collection { id }}}}`;
 
 // Step 3: Set your Endpoint
-const endpoint = 'https://subgraph.arthera.net/subgraphs/name/arthera/subscribers';
+const endpoint = 'https://subgraph.arthera.net/subgraphs/name/nfts2me/factory';
 
 // Step 4: Create an ApolloClient
 const client = new ApolloClient({
@@ -15,10 +15,10 @@ const client = new ApolloClient({
 
 async function executeQuery() {
 // Step 5: Execute the Query
-  let response = await client.query({ query: gql(NGP_QUERY), variables: { address: '0x476E2651BF97dE8a26e4A05a9c8e00A6EFa1390c' } });
+  let response = await client.query({ query: gql(NFT_QUERY), variables: { address: '0x476E2651BF97dE8a26e4A05a9c8e00A6EFa1390c' } });
 // Step 6:  Print the Result
   const data = response?.data;
-  const value = function (data) { if (data?.noGasPasses?.length > 0) { return 1; } return 0; }(data);
+  const value = function (data) { if (data?.nftcollector?.nftsFromCollection?.length > 0) { return 1; } return 0; }(data);
   console.log(value);
 }
 
