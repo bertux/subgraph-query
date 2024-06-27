2f86973f06d664190f3e6403e53618d5d3496e6b
Bertrand Juglas <b@juglas.name>
2024-06-27 12:28:21 +0200

    ZNS domain minting check

--- index.ts
+++ index.ts
@@ -2,10 +2,10 @@
 import {ApolloClient, InMemoryCache, gql} from '@apollo/client';
 
 // Step 2:  Define your Query
-export const NGP_QUERY = `query info($address: Bytes) {   noGasPasses(where: {creator: $address}) {     id   } }`;
+export const GQL_QUERY = `query info($address: Bytes) {domains(where: {creator: $address}) {id}}`;
 
 // Step 3: Set your Endpoint
-const endpoint = 'https://subgraph.arthera.net/subgraphs/name/arthera/subscribers';
+const endpoint = 'https://subgraph.arthera.net/subgraphs/name/arthera/zns';
 
 // Step 4: Create an ApolloClient
 const client = new ApolloClient({
@@ -15,10 +15,10 @@ const client = new ApolloClient({
 
 async function executeQuery() {
 // Step 5: Execute the Query
-  let response = await client.query({ query: gql(NGP_QUERY), variables: { address: '0x476E2651BF97dE8a26e4A05a9c8e00A6EFa1390c' } });
+  let response = await client.query({ query: gql(GQL_QUERY), variables: { address: '0x4FB7cD1cf8CFee6B9440f5ccDD2Cb43f73d5Aaa3' } });
 // Step 6:  Print the Result
   const data = response?.data;
-  const value = function (data) { if (data?.noGasPasses?.length > 0) { return 1; } return 0; }(data);
+  const value = function (data) { if (data?.domains?.length > 0) { return 1; } return 0; }(data);
   console.log(value);
 }
 
