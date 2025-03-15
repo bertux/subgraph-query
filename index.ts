a6d5d24e64595e32a25f84c4d1b605dc694c1da7
Bertrand Juglas <b@juglas.name>
2025-03-15 16:40:29 +0100

    refactor query to fetch batch auction bids and update endpoint

--- index.ts
+++ index.ts
@@ -1,17 +1,22 @@
 // Step 1:  Import the Dependencies
-import {ApolloClient, InMemoryCache, gql} from '@apollo/client';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
 // Step 2:  Define your Query
-export const SWAP_QUERY = `
-  query info {
-    swaps(first: 1, where: {origin: "0x568ba215891517462E6BEbAdAe41efa03fccbaC9"}) {
-      origin
+export const BIDS_QUERY = `
+  query getBatchAuctionLot($id: ID!) {
+    batchAuctionLot(id: $id) {
+      bids(first: 1000, where: {status: "claimed"}) {
+        bidId
+        bidder
+        rawAmountIn
+        referrer
+      }
     }
   }
 `;
 
 // Step 3: Set your Endpoint
-const endpoint = 'https://subgraph-test.arthera.net/subgraphs/name/cryptoalgebra/algebra';
+const endpoint = 'https://api.goldsky.com/api/public/project_cm87g2s71jd5j010598yx65ww/subgraphs/axis-origin-base-sepolia/1.0.6/gn';
 
 // Step 4: Create an ApolloClient
 const client = new ApolloClient({
@@ -20,10 +25,10 @@ const client = new ApolloClient({
 });
 
 async function executeQuery() {
-// Step 5: Execute the Query
-  let response = await client.query({ query: gql(SWAP_QUERY) });
-// Step 6:  Print the Result
-  console.log(response?.data?.swaps?.length)
+  // Step 5: Execute the Query
+  let response = await client.query({ query: gql(BIDS_QUERY), variables: { id: 'base-sepolia-0xba0000c28179ce533233a943d432eddd154e62a3-36' } });
+  // Step 6:  Print the Result
+  console.log(response?.data?.batchAuctionLot?.bids)
 }
 
 executeQuery();
